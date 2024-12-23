ff28594a7cce1bd964f3e5579c8a7c57deeed218
Qiushawa <qiushawa@gmail.com>
2024-12-23 11:20:13 +0800

    新增 Discord 機器人上線狀態及簡單回應功能

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ from dotenv import load_dotenv
 # load environment variables
 load_dotenv()
 client = commands.Bot(command_prefix = '.', intents = discord.Intents.all())
+# on ready
+@client.event
+async def on_ready():
+    # status
+    await client.change_presence(activity=discord.Game(name="我成功上線了!!!!"))
+
+# on message
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('.hello'):
+        await message.channel.send('Hello!')
 # run
 client.run(os.getenv('TOKEN'))
